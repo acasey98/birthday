afd0621ff2143cf9b5f883e741b0ef346a037b3e
Austin Casey <thepoast@gmail.com>
2019-05-20 21:27:54 -0500

    logout

---
+++ src/javascripts/components/myNavbar/myNavbar.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+const navbarEvents = () => {
+  const navLinks = document.getElementsByClassName('nav-link');
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].addEventListener('click', (e) => {
+      if (e.target.id === 'navbar-button-logout') {
+        firebase.auth().signOut()
+          .then(() => {
+            console.error('bye');
+          })
+          .catch(err => console.error('no', err));
+      }
+    });
+  }
+};
+
+export default { navbarEvents };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,11 +6,18 @@ import apiKeys from './helpers/apiKeys';
 
 import print from './components/auth/auth';
 
+import navbar from './components/myNavbar/myNavbar';
+
+import authData from './helpers/data/authData';
+
+
 const init = () => {
   console.error('keys', apiKeys.firebaseKeys);
   firebase.initializeApp(apiKeys.firebaseKeys);
   print.authPrintToDom('auth');
   print.birthdayPrintToDom('birthday');
+  navbar.navbarEvents();
+  authData.checkLoginStatus();
 };
 
 init();
