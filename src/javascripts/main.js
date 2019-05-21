41ff3a96a109e50f16edffc0c8ba9de43ba76878
Austin Casey <thepoast@gmail.com>
2019-05-20 19:54:57 -0500

    init firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,14 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import firebase from 'firebase/app';
+
+import apiKeys from './helpers/apiKeys';
+
 import print from './components/auth';
 
 const init = () => {
+  console.error('keys', apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseKeys);
   print.printToDom('auth');
   print.printToDom('birthday');
 };
