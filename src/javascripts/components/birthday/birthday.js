6c4b2d05e7b8f6818f1711999a43855f4f32cd56
Austin Casey <thepoast@gmail.com>
2019-05-25 11:28:35 -0500

    printstuff

--- src/javascripts/components/birthday/birthday.js
+++ src/javascripts/components/birthday/birthday.js
@@ -3,10 +3,11 @@ import util from '../../helpers/util';
 import birthdayData from '../../helpers/data/birthdayData';
 
 const birthdayPrintToDom = (uid) => {
-  birthdayData.getBirthdayByUid(uid).then((resp) => {
-    console.error(resp);
+  birthdayData.getBirthdayByUid(uid).then((birthday) => {
     let domString = '';
-    domString += '<h3>birthday</h3>';
+    domString += `<h3>${birthday.date}</h3>`;
+    domString += `<img src=${birthday.imageUrl} alt="birthday location"/>`;
+    domString += `<p>${birthday.location} @ ${birthday.time}`;
     util.printToDom('birthday', domString);
   }).catch(err => console.error('couldnt get birthday', err));
 };
